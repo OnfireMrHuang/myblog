f38208c89e484e53cc166caf09f27e86617097d9
OnfireMrHuang <engineer_mrhuang@163.com>
2020-08-24 12:50:50 +0800

    设置首页

---
+++ blog-app/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  	<el-row type="flex" justify="center">
+  		<el-col :span="24" class="footer">
+			<div>COPYRIGHT 2020 JESSIE. ALL RIGHTS RESERVED.</div>
+			<div>备号待定</div>
+  		</el-col>
+  	</el-row>
+  </div>
+</template>
+
+<style lang="less">
+	.footer {
+		display:flex;
+		justify-content:center;
+		align-items:center;
+		margin-top:8rem;
+		height:60px;
+		background:#23282D;
+		color:#666;
+		text-align:center;
+		line-height:60px;
+	}
+</style>
--- blog-app/pages/index.vue
+++ blog-app/pages/index.vue
@@ -1,82 +1,103 @@
 <template>
-  <div class="container">
-    <div>
-      <app-logo />
-      <h1 class="title">
-        小五的博客
-      </h1>
-      <h2 class="subtitle">
-        nuxt工程
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<div>
+	<nav-header :active="active"></nav-header>
+	<!-- 左侧文章内容 -->
+	<el-row type="flex" justify="center" class="content-blog">
+		<el-col :span="10">
+			<nuxt-link  v-for="item in list" :key="item._id" :to="{name:'Front-id',params:{id:item._id}}" class="box-href">
+				<el-card class="box-card" shadow="hover">
+					<h2 class="box-title">{{item.title}}</h2>
+					<div class="box-icon">
+						<span><i class="el-icon-date"></i>&nbsp;{{item.time}}</span>
+						<!-- <span><i class="el-icon-view"></i>&nbsp;115次阅读</span> -->
+					</div>
+					<div class="box-content">{{item.des}}</div>
+				</el-card>
+			</nuxt-link>
+			<el-pagination class="pagination" @current-change="pagination" background layout="prev, pager, next" :page-size="5" :total="count" v-show="count >= 5"></el-pagination>
+		</el-col>
+		<!-- 右侧关于我 -->
+        <el-col :span="5" :offset="1">
+			<el-card class="about">
+				<div class="about-title">about Me</div>
+				<div class="about-content">
+					<p>网名：Jessie</p>
+
+					<p>职业：Go后端工程师</p>
+
+					<p>邮箱：engineer_mrhuang@163.com</p>
+				</div>
+			</el-card>
+			<!-- 近期文章开始 -->
+			<el-card class="article">
+				<div class="article-title">近期文章</div>
+				<hr>
+				<nuxt-link v-for="item in lately" :key="item._id" :to="{name:'Front-id',params:{id:item._id}}" class="article-link">
+					<i class="el-icon-edit"></i>&nbsp;&nbsp;{{item.title}}
+				</nuxt-link>
+			</el-card>
+			<!-- 近期文章结束 -->
+
+			<!-- 友情链接开始 -->
+			<el-card class="link">
+				<div class="link-title">友情链接</div>
+				<hr>
+				<div class="link-content">
+					<a href="/" target="_blank" class="link-url">虚位以待</a>
+					<a href="/" target="_blank" class="link-url">虚位以待</a>
+				</div>
+			</el-card>
+			<!-- 友情链接结束 -->
+		</el-col>
+	</el-row>
+	<nav-footer />
+</div>
 </template>
 
 <script>
-import AppLogo from "../components/AppLogo.vue"
-
+import NavHeader from '~/components/NavHeader.vue'
+import NavFooter from '~/components/Footer.vue'
+// import {baseurl} from '~/plugins/url.js'
 export default {
-  components: {
-    AppLogo
-  }
+	data() {
+		return {
+			active:'index'
+		}
+	},
+	async asyncData({app}) {
+		// 服务器端渲染数据
+		// let json = {page:1,pagesize:5}
+		// let {data} =await app.$axios.get(`${baseurl}/api/article/getFrontArticle`,{params:json});
+		// let {list,count} = data;
+		// let lately = list.slice(0,4);
+		// return {list,count,lately}
+	},
+	methods: {
+		pagination(page) {
+			// let json = {page,pagesize:5}
+			// this.$axios.get(`${baseurl}/api/article/getFrontArticle`,{params:json}).then(res=>{
+			// 	let {error,count,list} = res.data;
+			// 	this.list =list;
+			// });
+		}
+	},
+	components: {
+		NavHeader,
+		NavFooter
+	},
+	head() {
+		return {
+			title:'Jessie的个人博客',
+			meta:[
+				{hid:'description',name:'description',content:''},
+				{hid:'keywords',name:'keywords',content:''},
+				{hid:'author',content:'jessie'}
+			]
+		}
+	}
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="less">
+    @import './../assets/css/Index/Content.less';
 </style>
