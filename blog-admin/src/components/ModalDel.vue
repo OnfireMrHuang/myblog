e18beafceb3589beb6e0715c1a09343d9e8f03aa
OnfireMrHuang <engineer_mrhuang@163.com>
2020-09-08 13:03:39 +0800

    提交模态对话框

---
+++ blog-admin/src/components/ModalDel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-dialog
+    title="提示"
+    :visible.sync="dialogVisible"
+    width="30%"
+    :before-close="handleClose">
+    <div style="text-align:center">
+        <span style="font-weight: bold;">这个操作将会删除您的数据</span>
+        <span style="font-weight: bold;">您确定要这么做？</span>
+    </div>
+    <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        dialogVisible: false
+      };
+    },
+    methods: {
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      }
+    }
+  };
+</script>
