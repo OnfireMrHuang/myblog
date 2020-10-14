57aa61ca9fb8361d41897c36ae2d49e00231dc83
OnfireMrHuang <engineer_mrhuang@163.com>
2020-10-14 14:06:46 +0800

    提交文章模块

--- server/routers/router.go
+++ server/routers/router.go
@@ -15,14 +15,23 @@ func InitRouter() *gin.Engine {
 	{
 		// 获取标签列表
 		apiV1.GET("/tags", v1.GetTags)
-
 		// 添加标签
 		apiV1.POST("/tags", v1.AddTag)
-
 		// 修改标签
 		apiV1.PUT("/tags/:id", v1.EditTag)
 		// 删除标签
 		apiV1.DELETE("/tags/:id", v1.DeleteTag)
+
+		// 获取文章列表
+		apiV1.GET("/articles", v1.GetArticles)
+		// 获取指定文章
+		apiV1.GET("/articles/:id", v1.GetArticle)
+		// 添加文章
+		apiV1.POST("/articles", v1.AddArticle)
+		// 编辑文章
+		apiV1.PUT("/articles/:id", v1.EditArticle)
+		// 删除文章
+		apiV1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 	return r
 }
