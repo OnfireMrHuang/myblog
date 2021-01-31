888d306f5ac2dc40ea6a4b1ed879a61bb65caed1
huangwenwu <xiaowu@mail.btc38.com>
2021-01-31 15:44:25 +0800

    提交代码

--- blog-app/pages/index.vue
+++ blog-app/pages/index.vue
@@ -31,7 +31,7 @@
 			<el-card class="article">
 				<div class="article-title">近期文章</div>
 				<hr>
-				<nuxt-link v-for="item in lately" :key="item._id" :to="{name:'article-front-id',params:{id:item._id}}" class="article-link">
+				<nuxt-link v-for="item in lately" :key="item.id" :to="{name:'article-id',params:{id:item.id}}" class="article-link">
 					<i class="el-icon-edit"></i>&nbsp;&nbsp;{{item.title}}
 				</nuxt-link>
 			</el-card>
